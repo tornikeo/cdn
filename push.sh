4c759573aa85ed2a2272d150ae7c744b0ecfb452
tornikeo <tornikeonoprishvili@gmail.com>
2025-09-07 19:08:00 +0200

    Update file at Sun 07 Sep 2025 07:08:00 PM CEST

--- push.sh
+++ push.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
-# Find untracked files and upload each
-for FILE in $(git ls-files --others --exclude-standard); do
+# Find untracked and modified files and upload each
+FILES=$(git ls-files --others --exclude-standard)
+FILES+=$(git ls-files --modified)
+
+for FILE in $FILES; do
   git add "$FILE" > /dev/null 2>&1
-  git commit -m "Add new file at $(date)" > /dev/null 2>&1
+  git commit -m "Update file at $(date)" > /dev/null 2>&1 || true
   git push > /dev/null 2>&1
   if [ $? -eq 0 ]; then
     echo "File uploaded to: https://raw.githubusercontent.com/tornikeo/cdn/master/$FILE"
